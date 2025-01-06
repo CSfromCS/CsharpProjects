ab67e8e6def0a58c5278f0237ada904201bb66ca
Christian Sarabia <csfromcs@gmail.com>
2025-01-06 23:46:00 +1100

    feat: implement sales file aggregation and create totals directory with initial file

--- DotNetPractice/mslearn-dotnet-files/Program.cs
+++ DotNetPractice/mslearn-dotnet-files/Program.cs
@@ -1,4 +1,13 @@
-﻿IEnumerable<string> FindFiles(string folderName)
+﻿var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);     
+var salesFiles = FindFiles(storesDirectory);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+IEnumerable<string> FindFiles(string folderName)
 {
     List<string> salesFiles = new List<string>();
 
@@ -6,19 +15,12 @@
 
     foreach (var file in foundFiles)
     {
-        // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
     }
 
     return salesFiles;
-}
-
-var salesFiles = FindFiles("stores");
-
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
 }
