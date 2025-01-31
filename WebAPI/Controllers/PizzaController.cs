19be3510d9bbe68e3e7f6e5341854fc10c14a5e9
Christian Sarabia <csfromcs@gmail.com>
2025-02-01 09:27:49 +1100

    feat: add DELETE action to PizzaController for removing pizzas

--- WebAPI/Controllers/PizzaController.cs
+++ WebAPI/Controllers/PizzaController.cs
@@ -53,4 +53,16 @@ public class PizzaController : ControllerBase
     }
 
     // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
